93d9561f852b7f507fdd769a1dbcd710dcf5e64a
pehprado <pedro.pradoliveira@gmail.com>
2018-06-13 00:04:58 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace pocAWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter writer = System.IO.File.AppendText("poc.txt")) {
+                writer.WriteLine($"{DateTime.Now}");
+            }
+        }
+    }
+}
